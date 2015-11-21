3e462cf1cecc3e3dbdaf1d6826472df86d33d90d
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2015-11-20 21:07:49 -0500

    added js script for button and css styles

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,18 @@
-$(document).ready(function() {
-    alert("hi!");
-  });
+// $(document).ready(function() {
+//     alert("hi!");
+//   });
+
+var deleteBtn = document.getElementById("delete-btn")
+var completeItem = document.getElementById("completed-item")
+var checkBox = document.getElementById("check-box")
+var deleteLine = document.getElementById("deleteBtn")
+
+
+deleteBtn.addEventListener("click", deleteRow);
+ 
+ function deleteRow () {
+    alert("delete!");
+ }
+
+
 
