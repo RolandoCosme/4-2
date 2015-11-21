51e4180ad6dffc45f256b4d1adb6617f6a1df026
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2015-11-20 22:01:22 -0500

    added second beta test file for js and html

--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,31 @@
-// $(document).ready(function() {
-//     alert("hi!");
-//   });
+$(document).ready(function(){
+  $(".btn-danger").hide();
 
 var deleteBtn = document.getElementById("delete-btn")
 var completeItem = document.getElementById("completed-item")
 var checkBox = document.getElementById("check-box")
-var deleteLine = document.getElementById("deleteBtn")
-
 
 deleteBtn.addEventListener("click", deleteRow);
  
+
+ // this will delete completed items but not the last item left
+
  function deleteRow () {
-    alert("delete!");
+    console.log("delete!");
  }
 
+ $('<input>').attr({
+    type: 'hidden',
+    id: 'add-item',
+    name: 'bar'
+}).appendTo('form');
 
 
+
+// $('#tableid').on('click', buttonSelector, function(){
+//     $(this).closest ('tr').remove ();
+// });
+
+for ( var i=0; i < deleteBtn.length; i++) { 
+console.log(deleteBtn) 
+}
