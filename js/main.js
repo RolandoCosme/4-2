d20a3227c3c5b0a3d9acf994f4319583b5f4d3e8
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2015-11-22 13:06:54 -0500

    cleaned html layout and added new js file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  //$(".btn-danger").hide();
+});
+
+
+var deleteBtn = document.getElementById("delete-btn")
+var completeItem = document.getElementById("completed-item")
+var checkBox = document.getElementById("check-box")
+var newRow = $("<tr>"); 
